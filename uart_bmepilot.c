04af44d9bef219c9d21392f7fc02d4e5ff1281a3
Igarashi <igarashi@bme.en.a.u-tokyo.ac.jp>
2019-02-05 18:12:14 +0900

    code was shrinked

--- uart_bmepilot.c
+++ uart_bmepilot.c
@@ -51,6 +51,7 @@ int main(int argc, char **argv){
     int pre_time = 0;
     int time_interval = 1000000; //1 second
     mavlink_message_t msg;
+    uint8_t test;
     mavlink_status_t status;
 
     // GNSS variable
@@ -84,8 +85,18 @@ int main(int argc, char **argv){
 
         // write with time_interval
         if (microsSinceEpoch() - pre_time > time_interval){
+            //mavlink_msg_heartbeat_pack(1, 1, &msg, MAV_TYPE_GROUND_ROVER, MAV_AUTOPILOT_ARDUPILOTMEGA, 
+            //                           0, 0, MAV_STATE_ACTIVE);
+            //len = mavlink_msg_to_send_buffer(buf, &msg);
+            //bytes_sent = write(fd, buf, len);
+
             mavlink_msg_heartbeat_pack(1, 1, &msg, MAV_TYPE_GROUND_ROVER, MAV_AUTOPILOT_ARDUPILOTMEGA, 
                                        0, 0, MAV_STATE_ACTIVE);
+            len = mavlink_msg_to_send_buffer(buf, &msg);
+            bytes_sent = write(fd, buf, len);
+
+            /* Send Status */
+            mavlink_msg_sys_status_pack(1, 200, &msg, 1, 1, 1, 500, 11000, -1, -1, 0, 0, 0, 0, 0, 0);
             len = mavlink_msg_to_send_buffer(buf, &msg);
             bytes_sent = write(fd, buf, len);
 
@@ -95,65 +106,146 @@ int main(int argc, char **argv){
                                          65535, 65535, satellites);
             len = mavlink_msg_to_send_buffer(buf, &msg);
             bytes_sent = write(fd, buf, len);
+
+            pre_time = microsSinceEpoch();
         }
         // Mission Request
-        if (mission_total_seq > 0 && pre_mission_seq != mission_seq){
+        //if (mission_total_seq > 0 && pre_mission_seq != mission_seq){
+        //    mavlink_msg_mission_request_int_pack(1, 200, &msg, 0, 0, mission_seq);
+        //    len = mavlink_msg_to_send_buffer(buf, &msg);
+        //   bytes_sent = write(fd, buf, len);
+        //    pre_mission_seq = mission_seq;
+        //}
+
+        if (mission_total_seq > 0){
             mavlink_msg_mission_request_int_pack(1, 200, &msg, 0, 0, mission_seq);
             len = mavlink_msg_to_send_buffer(buf, &msg);
             bytes_sent = write(fd, buf, len);
-            pre_mission_seq = mission_seq;
         }
+
 
         // receive section
         len = read(fd, buf, BUFFER_LENGTH);
-        if(len > 0){
-            for(i=0; i<len; i++){
-                printf("%02X", buf[i]);
-                if (mavlink_parse_char(MAVLINK_COMM_0, buf[i], &msg, &status)){
-                    // Packet decode
-                    printf("\nReceived packet: SYS: %d, COMP: %d, LEN: %d, MSG ID: %d\n", 
-                           msg.sysid, msg.compid, msg.len, msg.msgid);
-                }
-            }
+        if(len <= 0){
+            continue;
+        }
 
-            // mission count decoder
-            if (msg.msgid == 44){
-                mavlink_mission_count_t mavmc;
-                printf("mission count was received\n");
-                
-                mavlink_msg_mission_count_decode(&msg, &mavmc);
-                mission_total_seq = mavmc.count;
-                mission_seq = 0;
-                printf("%i\n", mission_total_seq);
-            }
+        for(i=0; i<len; i++){
+            //printf("%02X", buf[i]);
+            if (mavlink_parse_char(MAVLINK_COMM_0, buf[i], &msg, &status)){
+                // Packet decode
+                printf("\nReceived packet: SYS: %d, COMP: %d, LEN: %d, MSG ID: %d\n", 
+                       msg.sysid, msg.compid, msg.len, msg.msgid);
 
-            /* mission receiver */
-            if (msg.msgid == 73){
-                mavlink_mission_item_int_t mavmii;
-                //printf("mission item was received\n");
+                switch (msg.msgid){
+                    case 20:{
+                        printf("param read received\n");
+                        usleep(100000);
+                        mavlink_param_request_read_t mavprr;
+                        //printf("mission item was received\n");
 
-                // decode MISSION_ITEM_INT message
-                mavlink_msg_mission_item_int_decode(&msg, &mavmii);
-                float waypoint_x = mavmii.x/10000000.0;
-                float waypoint_y = mavmii.y/10000000.0;
-                printf("%i, %i, %i, %.9f, %.9f\n", mavmii.seq, mission_total_seq, mavmii.command,
-                       waypoint_x, waypoint_y);
-                    
-                // next mission sequence
-                mission_seq = mavmii.seq+1;
+                        // decode MISSION_ITEM_INT message
+                        mavlink_msg_param_request_read_decode(&msg, &mavprr);
+                        printf("%16s, %i",mavprr.param_id, mavprr.param_index);
 
-                // if mission sequence is end, send mission ack
-                if (mission_seq == mission_total_seq){
-                    mavlink_msg_mission_ack_pack(1, 200, &msg, 0, 0, MAV_MISSION_TYPE_MISSION);
-                    len = mavlink_msg_to_send_buffer(buf, &msg);
-                    bytes_sent = write(fd, buf, len);
+                        //mavlink_msg_param_value_pack(1, 1, &msg, "test", 1.0, MAV_PARAM_TYPE_UINT8, 1, 1);
+                        //len = mavlink_msg_to_send_buffer(buf, &msg);
+                        //bytes_sent = write(fd, buf, len);
+                        break;
+                    }
 
-                    mission_total_seq = 0;
+                    // param request received
+                    case 21:{
+                        printf("param request received\n");
+                        usleep(100000);
+
+                        mavlink_msg_param_value_pack(1, 1, &msg, "alpha", 1.0, MAV_PARAM_TYPE_UINT8, 2, 0);
+                        len = mavlink_msg_to_send_buffer(buf, &msg);
+                        bytes_sent = write(fd, buf, len);
+                        usleep(100000);
+                        mavlink_msg_param_value_pack(1, 1, &msg, "beta", 1.0, MAV_PARAM_TYPE_UINT8, 2, 1);
+                        len = mavlink_msg_to_send_buffer(buf, &msg);
+                        bytes_sent = write(fd, buf, len);
+                        break;
+                    }
+
+                    // mission count decoder
+                    case 44:{
+                        mavlink_mission_count_t mavmc;
+                        printf("mission count was received\n");
+            
+                        mavlink_msg_mission_count_decode(&msg, &mavmc);
+                        mission_total_seq = mavmc.count;
+                        mission_seq = 0;
+                        printf("%i\n", mission_total_seq);
+                        break;
+                    }
+
+                    /* mission receiver */
+                    case 73:{
+                        mavlink_mission_item_int_t mavmii;
+                        //printf("mission item was received\n");
+                        // decode MISSION_ITEM_INT message
+                        mavlink_msg_mission_item_int_decode(&msg, &mavmii);
+                        //int waypoint_x = mavmii.x;
+                        //int waypoint_y = mavmii.y;
+                        double waypoint_x = mavmii.x/10000000.0;
+                        double waypoint_y = mavmii.y/10000000.0;
+
+                        if (pre_mission_seq != mavmii.seq && mission_seq == mavmii.seq){
+                            //printf("%i, %i, %i, %.09f, %.09f\n", mavmii.seq, mission_total_seq, mavmii.command,
+                            //       waypoint_x, waypoint_y);
+                            printf("%i, %.09f, %.09f\n", mavmii.seq, waypoint_x, waypoint_y);
+                            pre_mission_seq = mavmii.seq;
+                            mission_seq = mavmii.seq+1;
+                        }
+                        // next mission sequence
+                        //if (mission_seq == mavmii.seq)
+                        //mission_seq = mavmii.seq+1;
+ 
+                        // if mission sequence is end, send mission ack
+                        if (mission_seq == mission_total_seq){
+                            mavlink_msg_mission_ack_pack(1, 200, &msg, 0, 0, MAV_MISSION_TYPE_MISSION);
+                            len = mavlink_msg_to_send_buffer(buf, &msg);
+                            bytes_sent = write(fd, buf, len);
+
+                            mission_total_seq = 0;
+                        }
+                        break;
+                    }
+
+                    case 76:{
+                        mavlink_command_long_t mavcl;
+
+                        // decode SET_MODE message
+                        mavlink_msg_command_long_decode(&msg, &mavcl);
+                        printf("%i, %i, %f, %f", mavcl.command, mavcl.confirmation, mavcl.param1, mavcl.param2);
+                        if (mavcl.command == MAV_CMD_REQUEST_PROTOCOL_VERSION){
+                            mavlink_msg_command_ack_pack(1, 1, &msg, mavcl.command,
+                                                         MAV_RESULT_ACCEPTED);
+                            len = mavlink_msg_to_send_buffer(buf, &msg);
+                            bytes_sent = write(fd, buf, len);
+                        }
+                        if (mavcl.command == MAV_CMD_REQUEST_PROTOCOL_VERSION){
+                            mavlink_msg_command_ack_pack(1, 1, &msg, mavcl.command,
+                                                         MAV_RESULT_ACCEPTED);
+                            len = mavlink_msg_to_send_buffer(buf, &msg);
+                            bytes_sent = write(fd, buf, len);
+
+                            mavlink_msg_autopilot_version_pack(1, 1, &msg, MAV_PROTOCOL_CAPABILITY_MISSION_INT,
+                                                               1, 1, 1, 1, &test, &test, &test, 1, 1, 1);
+                            len = mavlink_msg_to_send_buffer(buf, &msg);
+                            bytes_sent = write(fd, buf, len);
+                            break;
+                        }
+                    }            
                 }
             }
         }
+        //usleep(10000);
+        // param request received
+
         //printf("\n");
-        pre_time = microsSinceEpoch();
     }
 }
 
